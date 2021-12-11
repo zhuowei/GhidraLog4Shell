123ff2d3245e2d688b86a660f8637a356fc2210c
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2021-12-10 21:08:49 -0500

    add better payload

--- payload.py
+++ payload.py
@@ -1,1 +1,8 @@
-print "hello world!"
+import os
+import webbrowser
+
+for a in ["open -a Calculator", "gnome-calculator", "kcalc", "xcalc", "calc"]:
+  if os.system(a) == 0:
+    break
+else:
+  webbrowser.open("https://www.google.com/search?q=calculator")
